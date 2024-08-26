da3db13b15a11e9df05b59119d861c08b9560753
dongmin kim <147578617+kdmin1325@users.noreply.github.com>
2024-08-26 18:44:07 +0900

    Revert "."

--- source_codes/spring_boot_server/Main/src/main/java/com/example/main/domain/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.main.domain.user.service;
-
-import com.example.main.domain.user.request.SignupRequest;
-import com.example.main.global.exception.BusinessException;
-
-public interface UserService {
-    void registerUser(SignupRequest signupRequest) throws BusinessException;
-}
